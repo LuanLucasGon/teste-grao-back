00c13634fe7a7f93d31006371cc11b0ed7d65299
LuanLucasGon <lucasluangon@gmail.com>
2025-02-11 18:53:44 -0300

    feat: Creating user model

---
+++ src/models/UserModel.ts
@@ -0,0 +1,19 @@
+import mongoose, {Document, Schema} from "mongoose";
+
+export interface IUser extends Document {
+  name: string,
+  cpf:string,
+  email: string,
+  password: string,
+}
+
+const UserSchema = new Schema<IUser>({
+  name: {type: String, required: true},
+  cpf: {type: String, required: true, unique: true},
+  email: {tyoe: String, required: true},
+  password: {type: String, required: true}
+});
+
+const UserModel = mongoose.model<IUser>('users', UserSchema);
+
+export default UserModel;
