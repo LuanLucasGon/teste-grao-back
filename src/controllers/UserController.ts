cccf24d9bde3091f9b1a93c1d53a6b2e492d0c9b
LuanLucasGon <lucasluangon@gmail.com>
2025-02-11 20:27:24 -0300

    feat: creating user controller

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { UserRepository } from "../repositories/UserRepository";
+import { UserService } from "../services/UserService";
+
+const userRepository = new UserRepository();
+const userService = new UserService(userRepository);
+
+export class UserController{
+  static async getUser(req: Request, res: Response){
+    try {
+      const id = req.params.id;
+      const user = userService.getUser(id);
+      res.status(201).json({ msg: "Usuário encontrado", user });
+    } catch (error) {
+      res.status(500).json({msg: (error as Error).message});
+    }
+  }
+}
