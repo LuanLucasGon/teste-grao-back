fc7d7a7548020f567670556f8bdf168758028319
LuanLucasGon <lucasluangon@gmail.com>
2025-02-11 20:12:06 -0300

    feat: Creating AuthController

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { UserRepository } from "../repositories/UserRepository";
+import { AuthService } from "../services/AuthService";
+import { RegisterDTO, LoginDTO } from "../dtos/AuthDTO";
+
+const userRepository = new UserRepository();
+const authService = new AuthService(userRepository);
+
+export class AuthController{
+  static async register(req: Request, res: Response){
+    try {
+      const data: RegisterDTO = req.body;
+      const newUser = await authService.register(data);
+      res.status(201).json({ msg: "Usuário criado com sucesso!", user: newUser });
+    } catch (error) {
+      res.status(500).json({msg: (error as Error).message});
+    }
+  }
+
+  static async login(req: Request, res: Response){
+    try {
+      const data: LoginDTO = req.body;
+      const { token } = await authService.login(data);
+      res.status(201).json({ msg: "Autenticação realizada com sucesso!", token });
+    } catch (error) {
+      res.status(500).json({msg: (error as Error).message});
+    }
+  }
+}
